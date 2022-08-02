7c53fd0ce4e375b5e722c8b872dc5985ff8d2250
Felipe-Euzebio <felipe.euzebio.ti@gmail.com>
2022-08-02 16:13:32 -0300

    Tipos enumerados criados

---
+++ src/main/java/com/felipe/helpdeskapp/domain/enums/Prioridade.java
@@ -0,0 +1,45 @@
+package com.felipe.helpdeskapp.domain.enums;
+
+public enum Prioridade {
+
+	BAIXA(0, "BAIXA"), MEDIA(1, "MÉDIA"), ALTA(2, "ALTA");
+	
+	private Integer codigo;
+	private String descricao;
+	
+	
+	private Prioridade(Integer codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
+
+	
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	
+	public static Prioridade toEnum(Integer codigo){
+		
+		if(codigo == null) {
+			return null;
+		}
+		
+		for (Prioridade p : Prioridade.values()) {
+			
+			if(codigo.equals(p.getCodigo())) {
+				return p;
+			}
+			
+		}
+		
+		throw new IllegalArgumentException("Prioridade inválida");
+		
+	}
+	
+}
