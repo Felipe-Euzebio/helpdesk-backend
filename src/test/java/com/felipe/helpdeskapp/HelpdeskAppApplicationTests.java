6583ac2acf7c3cd9e9c762fd3425544f5c7c57aa
Felipe-Euzebio <felipe.euzebio.ti@gmail.com>
2022-08-02 15:42:26 -0300

    Iniciando o projeto

---
+++ src/test/java/com/felipe/helpdeskapp/HelpdeskAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.helpdeskapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
