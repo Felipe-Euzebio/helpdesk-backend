6176d8ce7eec860e32cb4c796c262ff3b449aa6f
Felipe-Euzebio <felipe.euzebio.ti@gmail.com>
2022-08-04 18:47:33 -0300

    Populando o banco de dados

--- src/main/java/com/felipe/helpdeskapp/services/DBService.java
+++ src/main/java/com/felipe/helpdeskapp/services/DBService.java
@@ -32,16 +32,32 @@ public class DBService {
 		Tecnico tecnico1 = new Tecnico(null, "Valdir Cezar", "381.880.360-99", "valdir@gmail.com", "123");
 		tecnico1.addPerfil(Perfil.ADMIN);
 		
+		Tecnico tecnico2 = new Tecnico(null, "Felipe Euzébio", "994.923.060-85", "felipe@gmail.com", "123");
+		tecnico2.addPerfil(Perfil.ADMIN);
+		
+		Tecnico tecnico3 = new Tecnico(null, "José Rômulo", "325.293.170-20", "joseromulo@gmail.com", "123");
+		tecnico3.addPerfil(Perfil.ADMIN);
+		
+		
 		Cliente cliente1 = new Cliente(null, "Linus Torvalds", "507.326.810-57", "torvalds@gmail.com", "123");
+		
+		Cliente cliente2 = new Cliente(null, "Steve Jobs", "880.338.890-71", "stevejobs@gmail.com", "123");
+		
+		Cliente cliente3 = new Cliente(null, "James Gosling", "770.082.580-70", "jamesgosling@gmail.com", "123");
+		
 		
 		Chamado chamado1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", tecnico1, cliente1);
 		
+		Chamado chamado2 = new Chamado(null, Prioridade.ALTA, Status.ENCERRADO, "Chamado 02", "Segundo chamado", tecnico2, cliente2);
 		
-		tecnicoRepository.saveAll(Arrays.asList(tecnico1));
+		Chamado chamado3 = new Chamado(null, Prioridade.BAIXA, Status.ABERTO, "Chamado 03", "Terceiro chamado", tecnico3, cliente3);
 		
-		clienteRepository.saveAll(Arrays.asList(cliente1));
 		
-		chamadoRepository.saveAll(Arrays.asList(chamado1));
+		tecnicoRepository.saveAll(Arrays.asList(tecnico1, tecnico2, tecnico3));
+		
+		clienteRepository.saveAll(Arrays.asList(cliente1, cliente2, cliente3));
+		
+		chamadoRepository.saveAll(Arrays.asList(chamado1, chamado2, chamado3));
 		
 	}
 	
