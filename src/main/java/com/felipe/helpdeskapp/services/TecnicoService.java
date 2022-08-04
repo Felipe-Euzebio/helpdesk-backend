60579a3c2bcccc1e59ae3bf9a83b917da5385723
Felipe-Euzebio <felipe.euzebio.ti@gmail.com>
2022-08-04 18:47:59 -0300

    Endpoint findAll para Tecnico

--- src/main/java/com/felipe/helpdeskapp/services/TecnicoService.java
+++ src/main/java/com/felipe/helpdeskapp/services/TecnicoService.java
@@ -1,5 +1,6 @@
 package com.felipe.helpdeskapp.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,12 @@ public class TecnicoService {
 		Optional<Tecnico> obj = tecnicoRepository.findById(id);
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado. Id: " + id));
+		
+	}
+
+	public List<Tecnico> findAll() {
+		
+		return tecnicoRepository.findAll();
 		
 	}
 	
