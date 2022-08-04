cb81450f42f96514cf3bd70f0dbf7c48e32294aa
Felipe-Euzebio <felipe.euzebio.ti@gmail.com>
2022-08-04 16:28:16 -0300

    Protegendo aplicação contra serialização

--- src/main/java/com/felipe/helpdeskapp/domain/Cliente.java
+++ src/main/java/com/felipe/helpdeskapp/domain/Cliente.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.felipe.helpdeskapp.domain.enums.Perfil;
 
 @Entity(name = "cliente")
@@ -16,6 +17,7 @@ public class Cliente extends Pessoa{
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<Chamado>();
 
