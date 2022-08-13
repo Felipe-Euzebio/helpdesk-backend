811cd84705137de1ab92358b26e075f134c18962
Felipe-Euzebio <felipe.euzebio.ti@gmail.com>
2022-08-13 17:39:31 -0300

    Habilitando o recurso CORS

--- src/main/java/com/felipe/helpdeskapp/HelpdeskAppApplication.java
+++ src/main/java/com/felipe/helpdeskapp/HelpdeskAppApplication.java
@@ -9,5 +9,14 @@ public class HelpdeskAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskAppApplication.class, args);
 	}
-
+	
+	
+	/*
+	 * @Bean public WebMvcConfigurer corsConfigurer() { return new
+	 * WebMvcConfigurer() {
+	 * 
+	 * @Override public void addCorsMappings(CorsRegistry registry) {
+	 * registry.addMapping("/**") .allowedOrigins("http://localhost:4200"); } }; }
+	 */
+	
 }
--- src/main/java/com/felipe/helpdeskapp/config/SecurityConfig.java
+++ src/main/java/com/felipe/helpdeskapp/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -16,13 +17,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import com.felipe.helpdeskapp.security.JWTAuthenticationFilter;
 import com.felipe.helpdeskapp.security.JWTAuthorizationFilter;
 import com.felipe.helpdeskapp.security.JWTUtil;
 
 @SuppressWarnings("deprecation")
+@Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
@@ -77,20 +78,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	}
 	
 	
-	@Bean
-	CorsConfigurationSource configurationSource() {
-		
-		//	Permissão de valores padrões
-		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
-		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
-		
-		//	Registrando configuração
-		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		
-		return source;
-		
-	}
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+    	
+        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+        
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        
+        return source;
+        
+    }
 	
 	
 	@Bean
