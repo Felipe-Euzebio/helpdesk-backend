03612fa02f445e9d086a2ca82983410e8b060007
Felipe-Euzebio <felipe.euzebio.ti@gmail.com>
2022-08-13 15:12:34 -0300

    Testando a propriedade CORS

--- src/main/java/com/felipe/helpdeskapp/resources/ClienteResource.java
+++ src/main/java/com/felipe/helpdeskapp/resources/ClienteResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import com.felipe.helpdeskapp.services.ClienteService;
 
 @RestController
 @RequestMapping(value = "/clientes")
+@CrossOrigin
 public class ClienteResource {
 
 	//	Injentando os serviços
