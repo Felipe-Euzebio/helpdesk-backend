83f816f61cd685e57a87cd63ceb83ab163b72e00
Felipe-Euzebio <felipe.euzebio.ti@gmail.com>
2022-08-19 16:07:44 -0300

    Anotações para testes

--- src/main/java/com/felipe/helpdeskapp/HelpdeskAppApplication.java
+++ src/main/java/com/felipe/helpdeskapp/HelpdeskAppApplication.java
@@ -2,6 +2,9 @@ package com.felipe.helpdeskapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class HelpdeskAppApplication {
@@ -16,7 +19,7 @@ public class HelpdeskAppApplication {
 	 * WebMvcConfigurer() {
 	 * 
 	 * @Override public void addCorsMappings(CorsRegistry registry) {
-	 * registry.addMapping("/**") .allowedOrigins("http://localhost:4200"); } }; }
-	 */
+	 * registry.addMapping("/**").allowedOrigins("http://localhost:4200"); } }; }
+	 */	 
 	
 }
